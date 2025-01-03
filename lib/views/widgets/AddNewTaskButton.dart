f834ab617c81a193b298673af0273dd634c892b5
Eslam Metawie <eslammetawie50@gmail.com>
2025-01-03 16:43:47 +0100

    waving button

--- lib/views/widgets/AddNewTaskButton.dart
+++ lib/views/widgets/AddNewTaskButton.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import '../../controllers/task_controller.dart';
 import 'SnackbarHelper.dart';
@@ -12,31 +13,57 @@ class AddNewTaskButton extends StatefulWidget {
   State<AddNewTaskButton> createState() => _AddNewTaskButtonState();
 }
 
-class _AddNewTaskButtonState extends State<AddNewTaskButton> {
+class _AddNewTaskButtonState extends State<AddNewTaskButton>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    )..repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Builder(
       builder: (scaffoldContext) {
-        return FloatingActionButton(
-          onPressed: () {
-            showDialog(
-              context: context,
-              builder: (context) {
-                return AddTaskDialog(
-                  onSave: (String title, String priority, String? descrebtion) {
-                    setState(() {
-                      widget.controller.addTask(title, priority, DateTime.now(), descrebtion);
-                      SnackbarHelper.showMissionCreatedSnackbar(scaffoldContext, title);
-                      Future.delayed(Duration(milliseconds: 500), () {
-                        Navigator.pop(context);
-                      });
-                    });
-                  },
-                );
-              },
+        return AnimatedBuilder(
+          animation: _controller,
+          builder: (context, child) {
+            return Transform.scale(
+              scale: 1 + 0.1 * sin(_controller.value * 2 * pi),
+              child: FloatingActionButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AddTaskDialog(
+                        onSave: (String title, String priority, String? descrebtion) {
+                          setState(() {
+                            widget.controller.addTask(title, priority, DateTime.now(), descrebtion);
+                            SnackbarHelper.showMissionCreatedSnackbar(scaffoldContext, title);
+                            Future.delayed(Duration(milliseconds: 500), () {
+                              Navigator.pop(context);
+                            });
+                          });
+                        },
+                      );
+                    },
+                  );
+                },
+                child: Icon(Icons.add),
+              ),
             );
           },
-          child: Icon(Icons.add),
         );
       },
     );
